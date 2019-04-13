daec1842ea286a44b811bc2c4c6191ef098accef
DESKTOP-TV7F15M\Reece PC <reecetucker2@gmail.com>
2019-04-13 20:20:42 +0100

    Implementing the functionality of the classes. PictureFragment - EditPhoto, is the method responsible for tweaking the bitmaps RGB. DrawCaption - Simply draws texts the user has inputted onto the Bitmap.
    Fragment Slider - Setting interfaces to the MainActivity and methods
    to keep track of the Seek bars value for each RGB.
    
    MainActivity - simple added the fragments to the xml layout

--- app/src/main/java/com/example/swipeeditlionheart/PictureFragment.java
+++ app/src/main/java/com/example/swipeeditlionheart/PictureFragment.java
@@ -1,18 +1,136 @@
 package com.example.swipeeditlionheart;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class PictureFragment extends Fragment {
+
+    // Member variable
+    private static EditText topCaption;
+    private static EditText bottomCaption;
+    private static ImageView imageView;
+    private static Button confimTextButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_slider_layout, container, false);
+
+        imageView = (ImageView) view.findViewById(R.id.imageView);
+        topCaption = (EditText) view.findViewById(R.id.top_caption);
+        bottomCaption = (EditText) view.findViewById(R.id.bottom_caption);
+        confimTextButton = (Button) view.findViewById(R.id.button_confim_text);
+
+        confimTextButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ConfimTextClicked(view);
+                    }
+                }
+        );
+
         return view;
+    }
+
+    // This method handles the confirm button being press and editing the Bitmap and put the text entered onto it
+    private void ConfimTextClicked(View view) {
+
+        Bitmap bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+        imageView.setImageBitmap(DrawCaption(bitmap, topCaption.getText().toString(), bottomCaption.getText().toString()));
+        Toast.makeText(getActivity(),"Press the save button to save this image", Toast.LENGTH_SHORT).show();
+    }
+
+    // This method will loop through all of the pixels in the Bitmap and tweak it by the RBG passed in
+    public Bitmap Editphoto(Bitmap original, int R, int G, int B /*int height, int width*/)
+    {
+        Bitmap editedImage = Bitmap.createBitmap(original.getWidth(),original.getHeight(),original.getConfig());
+
+        int pixelColor;
+        int height = original.getHeight();
+        int width = original.getWidth();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                pixelColor = original.getPixel(x,y);
+                int A = Color.alpha(pixelColor);
+                int Red = R + Color.red(pixelColor);
+                int Green = G + Color.green(pixelColor);
+                int Blue = B + Color.blue(pixelColor);
+
+                editedImage.setPixel(x, y, Color.argb(A,Red,Green,Blue));
+
+            }
+
+        }
+        return editedImage;
+    }
+
+    // This method will draw the Top and bottom caption onto the bit map onces the user has confimed their text
+    public Bitmap DrawCaption(Bitmap bitmap, String topCaption, String bottomCaption)
+    {
+        try{
+
+            Resources resources = getResources();
+            float scale = resources.getDisplayMetrics().density;
+            android.graphics.Bitmap.Config bitmapConfig =   bitmap.getConfig();
+            // set default bitmap config if none
+            if(bitmapConfig == null) {
+                bitmapConfig = android.graphics.Bitmap.Config.ARGB_8888;
+            }
+            // resource bitmaps are imutable,
+            // so we need to convert it to mutable one
+            bitmap = bitmap.copy(bitmapConfig, true);
+
+            Canvas canvas = new Canvas(bitmap);
+            // new antialised Paint
+            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            // text color - #3D3D3D
+            paint.setColor(Color.rgb(255,0, 0));
+            // text size in pixels
+            paint.setTextSize((int) (16 * scale));
+            // text shadow
+            paint.setShadowLayer(1f, 0f, 1f, Color.DKGRAY);
+
+            // draw the text to the top and the bottom of the image
+            Rect bounds = new Rect();
+            paint.getTextBounds(topCaption, 0, topCaption.length(), bounds);
+            paint.getTextBounds(bottomCaption, 0, bottomCaption.length(), bounds);
+            int xTop = (bitmap.getWidth() - bounds.width())/ 7;
+            int yTop = (bitmap.getHeight() + bounds.height())/ 20;
+            int xBottom = (bitmap.getWidth() - bounds.width()) / 7;
+            int yBottom = (bitmap.getHeight() + bounds.height())/ 5;
+
+            canvas.drawText(topCaption, xTop * scale, yTop * scale, paint);
+            canvas.drawText(bottomCaption, xBottom * scale, yBottom * scale, paint);
+
+            return bitmap;
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+
+    }
+
+    public void resetCaption()
+    {
+        topCaption.setText("");
+        bottomCaption.setText("");
     }
 }
--- app/src/main/java/com/example/swipeeditlionheart/SliderFragment.java
+++ app/src/main/java/com/example/swipeeditlionheart/SliderFragment.java
@@ -1,18 +1,149 @@
 package com.example.swipeeditlionheart;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SliderFragment extends Fragment {
+
+    // Member variables
+    private static SeekBar seekBar;
+    private static TextView textView;
+    private static ArrayList<SeekBar> seekbarlist =new ArrayList<SeekBar>();
+    private  static ArrayList<EditText> textViewlist =new ArrayList<EditText>();
+
+    // Creating an interface
+    SliderLister activityCommander;
+    public interface SliderLister
+    {
+        public void colourImage(int iRed, int iGreen, int iBlue);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            activityCommander = (SliderLister) activity;
+        }
+        catch (ClassCastException e)
+        {
+            throw new ClassCastException(activity.toString());
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_slider_layout, container, false);
+
+        //Setting up the ArrayList of Seekbars
+        seekbarlist.add((SeekBar) view.findViewById(R.id.seekbar_R));
+        seekbarlist.add((SeekBar) view.findViewById(R.id.seekbar_G));
+        seekbarlist.add((SeekBar) view.findViewById(R.id.seekbar_B));
+
+        //Setting up the ArrayList of Textviews
+        textViewlist.add((EditText) view.findViewById(R.id.seekbar_R_value));
+        textViewlist.add((EditText) view.findViewById(R.id.seekbar_G_value));
+        textViewlist.add((EditText) view.findViewById(R.id.seekbar_B_value));
+
+        // Setting up the Seek bars as will as listening for clicks
+        setSeekBar(seekbarlist, textViewlist, view);
+
         return view;
     }
+
+    public void setSeekBar(final ArrayList<SeekBar> seekBarlist, ArrayList<EditText> textList, final View view)
+    {
+        for (int i = 0; i < seekBarlist.size(); i++) {
+            for (int y = 0; y < textList.size(); y++) {
+                seekBar = view.findViewById((seekBarlist.get(i).getId()));
+                textView = view.findViewById(textList.get(y).getId());
+                textView.setText(seekBar.getProgress() + "/" + seekBar.getMax());
+
+                seekBar.setOnSeekBarChangeListener(
+
+                        new SeekBar.OnSeekBarChangeListener() {
+                            int progressValue;
+
+                            @Override
+                            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                                progressValue = progress;
+                                textView.setText(textView.getText().toString() + progress);
+
+                                switch (seekBar.getId()) {
+                                    case R.id.seekbar_R:
+                                        textView =  view.findViewById(R.id.seekbar_R_value);
+                                        textView.setText("R " + progress);
+                                        break;
+                                    case R.id.seekbar_G:
+                                        textView = view.findViewById(R.id.seekbar_G_value);
+                                        textView.setText("G " + progress);
+                                        break;
+                                    case R.id.seekbar_B:
+                                        textView = view.findViewById(R.id.seekbar_B_value);
+                                        textView.setText("B " + progress);
+                                        break;
+
+                                }
+                            }
+
+                            @Override
+                            public void onStartTrackingTouch(SeekBar seekBar) {
+
+                            }
+
+                            @Override
+                            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                                switch (seekBar.getId()) {
+                                    case R.id.seekbar_R:
+                                        textView = view.findViewById(R.id.seekbar_R_value);
+                                        textView.setText("R " + progressValue);
+                                        break;
+                                    case R.id.seekbar_G:
+                                        textView = view.findViewById(R.id.seekbar_G_value);
+                                        textView.setText("G " + progressValue);
+                                        break;
+                                    case R.id.seekbar_B:
+                                        textView = view.findViewById(R.id.seekbar_B_value);
+                                        textView.setText("B " + progressValue);
+                                        break;
+
+                                }
+                                // passing all of the seekbars values
+                                seekBarHasChanged(seekBarlist.get(0).getProgress(), seekBarlist.get(1).getProgress(), seekBarlist.get(2).getProgress());
+                            }
+                        }
+                );
+            }
+        }
+    }
+
+    public void seekBarHasChanged(int redProgress, int greenProgress, int blueProgress)
+    {
+        activityCommander.colourImage(redProgress, greenProgress, blueProgress);
+    }
+
+    // This method just simply resets this frgament with the base values
+    public void restSliders()
+    {
+        seekbarlist.get(0).setProgress(1);
+        seekbarlist.get(1).setProgress(1);
+        seekbarlist.get(2).setProgress(1);
+
+        textViewlist.get(0).setText(seekbarlist.get(0).getProgress() + "/" + seekBar.getMax());
+        textViewlist.get(1).setText(seekbarlist.get(1).getProgress() + "/" + seekBar.getMax());
+        textViewlist.get(2).setText(seekbarlist.get(2).getProgress() + "/" + seekBar.getMax());
+
+    }
+
 }
